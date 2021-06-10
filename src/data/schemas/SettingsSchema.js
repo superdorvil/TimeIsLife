105d6a9be1400dc4e9a253f02a11f9cb62f89e43
Dorvil Gabriel <dgabriel999@gmail.com>
2021-06-09 20:14:44 -0400

    changed default coor scheme

--- src/data/schemas/SettingsSchema.js
+++ src/data/schemas/SettingsSchema.js
@@ -1,9 +1,10 @@
 import {Schemas} from '_constants';
+import {Colors} from '_resources';
 
 const SettingsSchema = {
   name: Schemas.settings,
   properties: {
-    colorScheme: {type: 'int', default: 0},
+    colorScheme: {type: 'int', default: Colors.orangeDark},
     subTaskMode: {type: 'bool', default: false},
   },
 };
